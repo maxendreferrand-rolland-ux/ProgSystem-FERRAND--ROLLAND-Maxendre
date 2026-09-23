0089f84eefa0b11eebce37a71b861d19b121bc53
MAXENDRE FERRAND--ROLLAND <maxendre.ferrand--ro@iut.local>
2026-09-23 17:56:27 +0200

    A retravailler pour le 25/09

--- VFS/Utils.java
+++ VFS/Utils.java
@@ -1,18 +1,34 @@
 public class Utils {
 
-    public static int writeInt(byte[] memory, int offset, int value) {
+    public static byte[] writeInt(byte[] memory, int offset, int value) {
         memory[offset] = (byte) (value >> 24);
         memory[offset + 1] = (byte) (value >> 16);
         memory[offset + 2] = (byte) (value >> 8);
         memory[offset + 3] = (byte) value;
-        return 4;
+
+        return memory; 
     }
 
     public static int readInt(byte[] memory, int offset) {
-        return ((memory[offset] & 0xFF) << 24) |
-                ((memory[offset + 1] & 0xFF) << 16) |
-                ((memory[offset + 2] & 0xFF) << 8)  |
-                ((memory[offset + 3] & 0xFF));
+        int valeur1;
+        int valeur2;
+        int valeur3;
+        int valeur4;
+        int valeurFinale;
+
+        valeur1 = (int) memory[offset + 3](valeur1);
+        valeur1 = valeur1 << 24;
+
+        valeur2 = (int) memory[offset + 2](valeur2);
+        valeur2 = valeur2 << 16;
+
+        valeur3 = (int) memory[offset + 3](valeur3);
+        valeur3 = valeur3 << 8;
+
+        valeur4 = (int) memory[offset + 2](valeur4);
+        
+        valeurFinale = valeur1 | valeur2 | valeur3 |valeur4 ;
+        return valeurFinale;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
