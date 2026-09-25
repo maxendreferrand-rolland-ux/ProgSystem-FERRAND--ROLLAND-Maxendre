7f7ed404023519d991184597daeffd4f91a2bf1d
maxendreferrand-rolland-ux <maxendre.ferrand--rolland@iut-rodez.fr>
2026-09-25 19:44:36 +0200

    Testrunner.java TP2 revue, Utils.java et Testrunner.java à finir pour le TP3

--- VFS/TestRunner.java
+++ VFS/TestRunner.java
@@ -1,34 +1,39 @@
-public static void testStep2() {
-    System.out.println("=== TEST ÉTAPE 2 : Utils Entiers ===");
+public class TestRunner {
+    public static void main(String[] args) {
+        testStep2();
+    }
 
-    byte[] buffer = new byte[32];
+    public static void testStep2() {
+        System.out.println("=== TEST ÉTAPE 2 : Utils Entiers ===");
 
-    int value = 0xF0A1B2E3;
-    int written = Utils.writeInt(buffer, 3, value);
+        byte[] buffer = new byte[32];
 
-    assert written == 4 : "writeInt doit retourner 4";
+        int value = 0xF0A1B2E3;
+        int written = Utils.writeInt(buffer, 3, value);
 
-    assert (buffer[3]  & 0xFF) == 0xF0 : "Octet 0 incorrect";
-    assert (buffer[4]  & 0xFF) == 0xA1 : "Octet 1 incorrect";
-    assert (buffer[5]  & 0xFF) == 0xB2 : "Octet 2 incorrect";
-    assert (buffer[6]  & 0xFF) == 0xE3 : "Octet 3 incorrect";
+        assert written == 4 : "writeInt doit retourner 4";
 
-    assert Utils.readInt(buffer, 3) == value :
-            "Erreur writeInt / readInt";
+        assert (buffer[3] & 0xFF) == 0xF0 : "Octet 0 incorrect";
+        assert (buffer[4] & 0xFF) == 0xA1 : "Octet 1 incorrect";
+        assert (buffer[5] & 0xFF) == 0xB2 : "Octet 2 incorrect";
+        assert (buffer[6] & 0xFF) == 0xE3 : "Octet 3 incorrect";
 
-    short shortValue = (short) 0xF0A1;
-    int shortWritten = Utils.writeShort(buffer, 20, shortValue);
+        assert Utils.readInt(buffer, 3) == value :
+                "Erreur writeInt / readInt";
 
-    assert shortWritten == 2 : "writeShort doit retourner 2";
+        short shortValue = (short) 0xF0A1;
+        int shortWritten = Utils.writeShort(buffer, 20, shortValue);
 
-    assert (buffer[20] & 0xFF) == 0xF0 :
-            "Premier octet du short incorrect";
+        assert shortWritten == 2 : "writeShort doit retourner 2";
 
-    assert (buffer[21] & 0xFF) == 0xA1 :
-            "Deuxième octet du short incorrect";
+        assert (buffer[20] & 0xFF) == 0xF0 :
+                "Premier octet du short incorrect";
+        assert (buffer[21] & 0xFF) == 0xA1 :
+                "Deuxième octet du short incorrect";
 
-    assert Utils.readShort(buffer, 20) == shortValue :
-            "Erreur writeShort / readShort";
+        assert Utils.readShort(buffer, 20) == shortValue :
+                "Erreur writeShort / readShort";
 
-    System.out.println("[OK] Étape 2 validée !");
+        System.out.println("[OK] Étape 2 validée !");
+    }
 }
--- VFS/Utils.java
+++ VFS/Utils.java
@@ -24,14 +24,46 @@ public class Utils {
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        memory[offset]     = (byte) ((value >> 8)& 0xFF);
+
+        memory[offset] = (byte) ((value >> 8)& 0xFF);
         memory[offset + 1] = (byte) (value & & 0xFF);
+
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
+
         int valeur1 = (memory[offset] & 0xFF) << 8;
         int valeur2 = memory[offset + 1] & 0xFF;
+
         return (short) (valeur1 | valeur2);
+    }
+
+    public static int writeLong(byte[] memory, int offset, long value) {
+
+    memory[offset] = (byte) (value >> 56);
+    memory[offset + 1] = (byte) (value >> 48);
+    memory[offset + 2] = (byte) (value >> 40);
+    memory[offset + 3] = (byte) (value >> 32);
+    memory[offset + 4] = (byte) (value >> 24);
+    memory[offset + 5] = (byte) (value >> 16);
+    memory[offset + 6] = (byte) (value >> 8);
+    memory[offset + 7] = (byte) value;
+
+    return 8;
+
+    }
+    public static long readLong(byte[] memory, int offset) {
+
+        long valeur1 = (memory[offset] & 0xFFL) << 56;
+        long valeur2 = (memory[offset + 1] & 0xFFL) << 48;
+        long valeur3 = (memory[offset + 2] & 0xFFL) << 40;
+        long valeur4 = (memory[offset + 3] & 0xFFL) << 32;
+        long valeur5 = (memory[offset + 4] & 0xFFL) << 24;
+        long valeur6 = (memory[offset + 5] & 0xFFL) << 16;
+        long valeur7 = (memory[offset + 6] & 0xFFL) << 8;
+        long valeur8 = (memory[offset + 7] & 0xFFL);
+
+        return valeur1 | valeur2 | valeur3 | valeur4 | valeur5 | valeur6 | valeur7 | valeur8;
     }
 }
